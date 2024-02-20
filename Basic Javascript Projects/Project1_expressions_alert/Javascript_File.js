9de2553ba4c57dfba4ca2a6451370951c56f65b1
TeliRad <156611012+TeliRad@users.noreply.github.com>
2024-02-19 22:26:34 -0700

    Pizza.HTML

---
+++ Basic Javascript Projects/Project1_expressions_alert/Javascript_File.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Write title here</title>
+        <meta charset="utf-8"
+    </head>
+    <body>
+    <script src="Javascript File.js"></script>
+        <script>
+            window.alert("This is an alert message");
+        </script>
+        
+    </body>
+</html>
