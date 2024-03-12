39bf5394035abaa50cd61d4dbe6593434c080ec6
TeliRad <156611012+TeliRad@users.noreply.github.com>
2024-03-11 21:39:26 -0600

    Lightbox

---
+++ js/lightbox.js
@@ -0,0 +1,8 @@
+function openLightbox(imageSrc) {
+    document.getElementById('lightbox-img').src = imageSrc;
+    document.getElementById('lightbox').style.display = 'block';
+}
+
+function closeLightbox() {
+    document.getElementById('lightbox').style.display = 'none';
+}
